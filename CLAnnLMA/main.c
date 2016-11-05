1ef46eaf9e81e3011e0a452725d3b3b7fac5caa2
Albertomac <Albertomac@albertomacbook.homenet.telecomitalia.it>
2016-11-05 17:37:09 +0100

    Initial Commit

---
+++ CLAnnLMA/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CLAnnLMA
+//
+//  Created by Albertomac on 11/5/16.
+//  Copyright © 2016 Albertomac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
